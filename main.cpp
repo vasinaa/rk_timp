f4fa5c542d154157b78421d20ea3a7e23c0fd2a4
vasinaa <99878647+vasinaa@users.noreply.github.com>
2022-05-31 19:54:15 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <random>
+
+
+struct Tree {
+    int value = 0;
+    struct Tree *parent = nullptr;
+    struct Tree *left = nullptr;
+    struct Tree *right = nullptr;
+};
+
+
+void pLeft(int value, Tree *pTree) {
+    pTree->left = new Tree;
+    pTree->left->value = value;
+    pTree->left->parent = pTree;
+}
+
+void pRight(int value, Tree *pTree) {
+    pTree->right = new Tree;
+    pTree->right->value = value;
+    pTree->right->parent = pTree;
+}
+
+
+void show(Tree *tree, std::vector<int> &out) {
+    if (tree == nullptr) return;
+
+    show(tree->left, out);
+
+    show(tree->right, out);
+    out.push_back(tree->value);
+
+
+}
+
+
--- tests/test.cpp
+++ tests/test.cpp
@@ -7,7 +7,7 @@ TEST(Tree, Empty) {
     vector<int> out_expect = {0};
     vector<int> out;
     Tree *tree = new Tree{};
-    reverse(tree, out);
+    show(tree, out);
     EXPECT_EQ(out_expect, out);
 }
 
@@ -16,34 +16,34 @@ TEST(Tree, element_right) {
     vector<int> out_expect = {1, 0};
     vector<int> out;
     Tree *tree = new Tree{};
-    addRight(1, tree);
-    reverse(tree, out);
+    pRight(1, tree);
+    show(tree, out);
     EXPECT_EQ(out_expect, out);
 }
 
 TEST(Tree, FullTree) {
     vector<int> out;
-    vector<int> out_expect = { 204, 1037, 43, -43, 185, 12, 12, -1, 98, 543, 56};
+    vector<int> out_expect = { 125,538, 2, 115, 10, 18, 3, 56, 17, 45, 26};
 
 
-    Tree *tree = new Tree{56, nullptr, nullptr};
+    Tree *tree = new Tree{26, nullptr, nullptr};
 
-    addLeft(12, tree);
-    addRight(543, tree);
+    pLeft(17, tree);
+    pRight(45, tree);
 
-    addLeft(-1, tree->right);
-    addRight(98, tree->right);
+    pLeft(3, tree->right);
+    pRight(56, tree->right);
 
-    addLeft(43, tree->left);
-    addRight(12, tree->left);
+    pLeft(10, tree->left);
+    pRight(18, tree->left);
 
-    addLeft(-43, tree->left->right);
-    addRight(185, tree->left->right);
+    pLeft(2, tree->left->right);
+    pRight(115, tree->left->right);
 
-    addLeft(204, tree->left->left);
-    addRight(1037, tree->left->left);
+    pLeft(125, tree->left->left);
+    pRight(538, tree->left->left);
 
-    reverse(tree, out);
+    show(tree, out);
     EXPECT_EQ(out_expect, out);
 
 }

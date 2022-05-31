f33ef5d2bdc59d7f9a790aaa4b726de2f5d05ba4
vasinaa <99878647+vasinaa@users.noreply.github.com>
2022-05-31 21:20:47 +0300

    Update test.cpp

--- tests/test.cpp
+++ tests/test.cpp
@@ -23,25 +23,25 @@ TEST(Tree, element_right) {
 
 TEST(Tree, FullTree) {
     vector<int> out;
-    vector<int> out_expect = { 150, 545, 41, -25, 125, 11, 12, -3, 81, 412, 26};
+    vector<int> out_expect = { 250, 42, 1226, 35,-87, 18,171, 56,-35, 48, 69};
 
 
     Tree *tree = new Tree{26, nullptr, nullptr};
 
-    addl(10, tree);
-    addr(412, tree);
+    addl(35, tree);
+    addr(38, tree);
 
-    addl(-3, tree->right);
-    addr(81, tree->right);
+    addl(-35, tree->right);
+    addr(69, tree->right);
 
-    addl(41, tree->left);
-    addr(11, tree->left);
+    addl(42, tree->left);
+    addr(18, tree->left);
 
-    addl(-25, tree->left->right);
-    addr(125, tree->left->right);
+    addl(-87, tree->left->right);
+    addr(171, tree->left->right);
 
-    addl(150, tree->left->left);
-    addr(545, tree->left->left);
+    addl(250, tree->left->left);
+    addr(1226, tree->left->left);
 
     show(tree, out);
     EXPECT_EQ(out_expect, out);
